21ab371e5f62c8a65617ce66f2540c428b79bec7
ssprang <sara.sprang@gmail.com>
2013-01-16 14:01:45 +0100

    Initial commit of SEO plugin.

---
+++ src/main/java/com/atex/plugins/seoplugin/filter/SitemapFilter.java
@@ -0,0 +1,5 @@
+package com.atex.plugins.seoplugin.filter;
+
+public class SitemapFilter {
+
+}
---
+++ src/test/java/com/atex/plugins/seoplugin/MainElementControllerTest.java
@@ -0,0 +1,20 @@
+package com.atex.plugins.seoplugin;
+
+import junit.framework.TestCase;
+
+public class MainElementControllerTest extends TestCase {
+    
+    private MainElementController _testTarget;
+    
+    @Override
+    protected void setUp() {
+        _testTarget = new MainElementController();
+    }
+     
+    
+    public void testHelloGreenfieldTimesControllerConstruction()
+        throws Exception
+    {
+        assertNotNull("controller failed initialization", _testTarget);
+    }
+}
