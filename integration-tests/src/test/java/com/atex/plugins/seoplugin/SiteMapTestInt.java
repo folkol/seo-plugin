ee51f00a8604a7354ee535f7537ffb1b82ea9f95
Mattias Johansson <mattias.johansson@atex.com>
2013-03-12 09:43:57 +0100

    PPLAN-1493: Added integration test artifact

---
+++ integration-tests/src/test/java/com/atex/plugins/seoplugin/SiteMapTestInt.java
@@ -0,0 +1,17 @@
+package com.atex.plugins.seoplugin;
+
+import static junit.framework.TestCase.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.polopoly.testbase.TestBaseRunner;
+
+@RunWith(TestBaseRunner.class)
+public class SiteMapTestInt {
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+}
